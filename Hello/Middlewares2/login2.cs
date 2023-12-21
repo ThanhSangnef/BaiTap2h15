555cb0bdc152d377457b16b36b877af6d3fe5348
phuc <mp31121999@gmail.com>
2023-12-21 14:28:30 +0700

    create Middlewares2

---
+++ Hello/Middlewares2/login2.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello.Middlewares2
+{
+    internal class login2
+    {
+    }
+}
